60b9d1df6ae78ac63ccb465eadf3dccadf306bb1
David Bergh <berghdavid@hotmail.com>
2025-09-29 11:14:51 +0200

    Skip already converted

--- src/main.py
+++ src/main.py
@@ -37,7 +37,7 @@ def ftp_recursive_download(ftp: FTP, remote_dir: str, local_dir: str):
 
 def fetch_specs(release_v: str, ftp_user="", ftp_password=""):
     """Fetch all 3GPP specifications for a certain release version."""
-    print("Fetching 3GPP specifications...")
+    print(f"Fetching 3GPP v{release_v} specifications...")
     download_path = join(DOWNLOADS_DIR, release_v)
     os.makedirs(download_path, exist_ok=True)
 
@@ -49,7 +49,7 @@ def fetch_specs(release_v: str, ftp_user="", ftp_password=""):
 
 def unzip_dirs(release_v: str):
     """Unzip all .zip files in the downloads directory."""
-    print("Unzipping 3GPP specifications...")
+    print(f"Unzipping 3GPP v{release_v} specifications...")
     download_path = join(DOWNLOADS_DIR, release_v)
     for dirpath, _, filenames in os.walk(download_path):
         for filename in filenames:
@@ -65,34 +65,60 @@ def unzip_dirs(release_v: str):
                     print(f"Bad zip file: {zip_path}")
 
 
-def convert_docs(release_v: str, gotenberg_endpoint: str):
-    """Convert all .doc files to PDF in the extracted directory."""
-    print("Converting 3GPP specifications to PDF...")
+def convert_docs(release_v: str, gotenberg_endpoint: str, timeout: float):
+    """Convert all unconverted .doc files to PDF in the extracted directory."""
     gotenberg_url = f"{gotenberg_endpoint}/forms/libreoffice/convert"
     root_dir = join(DOWNLOADS_DIR, release_v)
+    print(f"Converting 3GPP v{release_v} specifications to PDF...")
+    print(f"\tConversion url: {gotenberg_url}")
+    print(f"\tTimeout:        {timeout:.2f}")
+
+    fails = []
+    pre_converted = []
+    converted = []
+    doc_files = 0
+    tot_files = 0
 
     for dirpath, _, filenames in os.walk(root_dir):
+        tot_files += len(filenames)
         for filename in filenames:
             lower = filename.lower()
-            if lower.endswith(".doc") or lower.endswith(".docx"):
-                doc_path = join(dirpath, filename)
+            if not (lower.endswith(".doc") or lower.endswith(".docx")):
+                continue
 
-                # Mirror directory structure inside pdf_root
-                pdf_dir = join(PDF_DIR, relpath(dirpath, root_dir))
-                os.makedirs(pdf_dir, exist_ok=True)
+            doc_files += 1
+            doc_path = join(dirpath, filename)
 
-                pdf_path = join(pdf_dir, filename + ".pdf")
+            # Mirror directory structure inside pdf_root
+            pdf_dir = join(PDF_DIR, relpath(dirpath, root_dir))
+            os.makedirs(pdf_dir, exist_ok=True)
+            pdf_path = join(pdf_dir, filename + ".pdf")
+            if os.path.isfile(pdf_path):
+                print(f"✅ PDF already exists: {doc_path} → {pdf_path}")
+                pre_converted.append(doc_path)
+                continue
 
-                with open(doc_path, "rb") as f:
-                    files = {"files": (filename, f, "application/msword")}
-                    response = post(gotenberg_url, files=files, timeout=60)
+            with open(doc_path, "rb") as f:
+                files = {"files": (filename, f, "application/msword")}
+                response = post(gotenberg_url, files=files, timeout=timeout)
 
-                if response.status_code == 200:
-                    with open(pdf_path, "wb") as pdf_file:
-                        pdf_file.write(response.content)
-                    print(f"✅ Converted: {doc_path} → {pdf_path}")
-                else:
-                    print(f"❌ Failed ({response.status_code}): {doc_path}")
+            if response.status_code == 200:
+                with open(pdf_path, "wb") as pdf_file:
+                    pdf_file.write(response.content)
+                print(f"✅ Converted: {doc_path} → {pdf_path}")
+                converted.append(doc_path)
+            else:
+                print(f"❌ Failed ({response.status_code} - {response.text}): {doc_path}")
+                fails.append(doc_path)
+    
+    print(f"Found {tot_files} files in total")
+    print(f"Found {doc_files} convertible files")
+    print(f"PDFs already existed for {len(pre_converted)} files")
+    print(f"PDFs were created for {len(converted)} files")
+    print(f"Conversion failed for {len(fails)} files")
+    if len(fails) > 0:
+        print(f"The following file conversions failed:\n{fails}")
+
 
 
 def get_parser() -> argparse.ArgumentParser:
@@ -109,6 +135,8 @@ def get_parser() -> argparse.ArgumentParser:
                         required=False, metavar='\b', help="Skip the unzipping stage.")
     parser.add_argument("-c", "--skip-convert", dest="skip_convert", type=bool, default=False,
                         required=False, metavar='\b', help="Skip the PDF conversion stage.")
+    parser.add_argument("-t", "--timeout", dest="timeout", type=float, default=60.0,
+                        required=False, metavar='\b', help="PDF conversion timeout.")
     return parser
 
 
@@ -117,10 +145,16 @@ def main():
     args = get_parser().parse_args()
     if not args.skip_fetch:
         fetch_specs(args.version)
+    else:
+        print("Skipping fetching as `--skip-fetch` was provided...")
     if not args.skip_unzip:
         unzip_dirs(args.version)
+    else:
+        print("Skipping unzipping as `--skip-unzip` was provided...")
     if not args.skip_convert:
-        convert_docs(args.version, args.gotenberg)
+        convert_docs(args.version, args.gotenberg, args.timeout)
+    else:
+        print("Skipping pdf-conversion as `--skip-convert` was provided...")
 
 
 if __name__ == "__main__":
